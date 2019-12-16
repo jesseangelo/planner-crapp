b8212157ccee60de1d86018c5178f1a3ba5ef946
Jesse Angelo <jesserangelo@gmail.com>
2019-12-15 23:59:23 -0500

    no state assign

--- src/App.js
+++ src/App.js
@@ -7,10 +7,6 @@ import PropTypes from 'prop-types';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      carbs: props.carbs,
-      proteins: props.proteins
-    };
     //this.tick = this.tick.bind(this);
   }
 
@@ -18,19 +14,19 @@ class App extends React.Component {
     return (
       <div className="container">
         <div className="row">
-          <Meal mealName="El Desayuno" carbList={this.state.carbs} proteinList={this.state.proteins} />
+          <Meal mealName="El Desayuno" carbList={this.props.carbs} proteinList={this.props.proteins} />
         </div>
         <br /><br />
         <div className="row">
-          <Meal mealName="El Lunch" carbList={this.state.carbs} proteinList={this.state.proteins} />
+          <Meal mealName="El Lunch" carbList={this.props.carbs} proteinList={this.props.proteins} />
         </div>
         <br /><br />
         <div className="row">
-          <Meal mealName="La Cena" carbList={this.state.carbs} proteinList={this.state.proteins} />
+          <Meal mealName="La Cena" carbList={this.props.carbs} proteinList={this.props.proteins} />
         </div>
         <br /><br />
         <div className="row">
-          <Meal mealName="La Merienda" carbList={this.state.carbs} proteinList={this.state.proteins} />
+          <Meal mealName="La Merienda" carbList={this.props.carbs} proteinList={this.props.proteins} />
         </div>
       </div>
     );
@@ -40,12 +36,12 @@ class App extends React.Component {
 App.propTypes = {
   carbs: PropTypes.array,
   proteins: PropTypes.array,
-  //fats: PropTypes.array
+  fats: PropTypes.array
 };
 App.defaultProps = {
   carbs: ['pasta', 'oatmeal', 'Lucky Charms'],
   proteins: ['pork', 'chicken', 'bison'],
- // fats: [],
+  fats: [],
 };
 
 export default App;
